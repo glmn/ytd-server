eb748c9d3c72a01ac45e9df1f03a02573ab79b40
Stanislav Gelman <speedster.ggg@gmail.com>
2017-03-15 14:43:50 +0300

    Test response query

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ server.listen(3000);
 
 io.on('connection', function(socket){
 	logger.log('Connected');
+
+	socket.on('nodes-request', function(){
+		var data = {
+			id: 1,
+			data: 'test',
+			uploaded: 0,
+			status: 'Making video'
+		}
+		io.emit('nodes-response', data);
+	})
+
+
+
 	socket.on('disconnect', function(socket){
 		logger.log('Disconnected');
 	})
